93893c8ccbc90f4de27c3fb8eee8e8b57f827e95
macbook <macbook@MacBook-Air-cua-macbook.local>
2024-05-30 15:43:39 +0700

    fix sign up

--- src/app/(starting_screen)/(auth)/(sign_up)/sign_up_1/[verify_email]/register_information/page.jsx
+++ src/app/(starting_screen)/(auth)/(sign_up)/sign_up_1/[verify_email]/register_information/page.jsx
@@ -1,7 +1,9 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./register_information.css";
 import { useRouter } from "next/navigation";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/bootstrap.css";
 const AWS = require("aws-sdk");
 
 AWS.config.update({
@@ -12,13 +14,371 @@ AWS.config.update({
 
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
+const location = [
+  {
+    province: "Aichi",
+    city: [
+      "Anjō",
+      "Atsuta",
+      "Gamagōri",
+      "Handa",
+      "Hekinan",
+      "Ichinomiya",
+      "Inazawa",
+      "Kariya",
+      "Kasugai",
+      "Komaki",
+      "Kōnan",
+      "Nagoya",
+      "Nishio",
+      "Okazaki",
+      "Seto",
+      "Tokoname",
+      "Toyohashi",
+      "Toyokawa",
+      "Toyota",
+    ],
+  },
+  {
+    province: "Akita",
+    city: ["Noshiro", "Ōdate"],
+  },
+  {
+    province: "Aomori",
+    city: ["Aomori", "Hachinohe", "Hirosaki"],
+  },
+  {
+    province: "Chiba",
+    city: [
+      "Chiba",
+      "Chōshi",
+      "Funabashi",
+      "Ichihara",
+      "Ichikawa",
+      "Kashiwa",
+      "Kisarazu",
+      "Matsudo",
+      "Narashino",
+      "Narita",
+      "Noda",
+      "Sawara",
+      "Urayasu",
+    ],
+  },
+  {
+    province: "Ehime",
+    city: ["Imabari", "Matsuyama", "Niihama", "Saijō", "Uwajima", "Yawatahama"],
+  },
+  {
+    province: "Fukui",
+    city: ["Sabae", "Takefu", "Tsuruga"],
+  },
+  {
+    province: "Fukuoka",
+    city: [
+      "Iizuka",
+      "Kitakyūshū",
+      "Kurume",
+      "Nōgata",
+      "Ōkawa",
+      "Ōmuta",
+      "Tagawa",
+    ],
+  },
+  {
+    province: "Fukushima",
+    city: ["Aizu-wakamatsu", "Iwaki", "Kōriyama"],
+  },
+  {
+    province: "Gifu",
+    city: ["Gifu", "Kakamigahara", "Ōgaki", "Tajimi", "Takayama", "Toki"],
+  },
+  {
+    province: "Gumma",
+    city: [
+      "Isesaki",
+      "Kiryū",
+      "Maebashi",
+      "Ōta",
+      "Takasaki",
+      "Tatebayashi",
+      "Tomioka",
+    ],
+  },
+  {
+    province: "Hokkaido",
+    city: [
+      "Asahikawa",
+      "Bibai",
+      "Chitose",
+      "Ebetsu",
+      "Hakodate",
+      "Iwamizawa",
+      "Kitami",
+      "Kushiro",
+      "Muroran",
+      "Nemuro",
+      "Noboribetsu",
+      "Obihiro",
+      "Otaru",
+      "Rumoi",
+      "Sapporo",
+      "Tomakomai",
+      "Wakkanai",
+      "Yūbari",
+    ],
+  },
+  {
+    province: "Hyōgo",
+    city: [
+      "Akashi",
+      "Akō",
+      "Amagasaki",
+      "Ashiya",
+      "Himeji",
+      "Itami",
+      "Kakogawa",
+      "Kawanishi",
+      "Kōbe",
+      "Miki",
+      "Nishinomiya",
+      "Takarazuka",
+      "Takasago",
+    ],
+  },
+  {
+    province: "Ibaraki",
+    city: [
+      "Hitachi",
+      "Hitachinaka",
+      "Kitaibaraki",
+      "Koga",
+      "Mito",
+      "Shimodate",
+      "Tsuchiura",
+      "Tsukuba Science City",
+    ],
+  },
+  {
+    province: "Iwate",
+    city: ["Kamaishi", "Miyako", "Mizusawa", "Morioka"],
+  },
+  {
+    province: "Kagawa",
+    city: ["Marugame", "Sakaide", "Takamatsu"],
+  },
+  {
+    province: "Kagoshima",
+    city: ["Kanoya", "Sendai"],
+  },
+  {
+    province: "Kanagawa",
+    city: [
+      "Atsugi",
+      "Chigasaki",
+      "Fujisawa",
+      "Hadano",
+      "Hakone",
+      "Hiratsuka",
+      "Kamakura",
+      "Kawasaki",
+      "Miura",
+      "Odawara",
+      "Sagamihara",
+      "Yamato",
+      "Yokohama",
+      "Yokosuka",
+    ],
+  },
+  {
+    province: "Kōchi",
+    city: ["Kōchi"],
+  },
+  {
+    province: "Kumamoto",
+    city: ["Arao", "Kumamoto", "Minamata", "Yatsushiro"],
+  },
+  {
+    province: "Kyōto",
+    city: ["Kyōto", "Maizuru", "Uji"],
+  },
+  {
+    province: "Mie",
+    city: ["Ise", "Kuwana", "Matsuzaka", "Suzuka", "Tsu", "Ueno", "Yokkaichi"],
+  },
+  {
+    province: "Miyagi",
+    city: ["Ishinomaki", "Kesennuma", "Sendai", "Shiogama"],
+  },
+  {
+    province: "Miyazaki",
+    city: ["Miyakonojō", "Miyazaki", "Nichinan", "Nobeoka"],
+  },
+  {
+    province: "Nagasaki",
+    city: ["Hirado", "Isahaya", "Nagasaki", "Ōmura", "Sasebo", "Shimabara"],
+  },
+  {
+    province: "Nagasaki",
+    city: ["Hirado", "Isahaya", "Nagasaki", "Ōmura", "Sasebo", "Shimabara"],
+  },
+  {
+    province: "Niigata",
+    city: ["Jōetsu", "Kashiwazaki", "Nagaoka", "Niigata", "Niitsu", "Sanjō"],
+  },
+  {
+    province: "Ōita",
+    city: ["Beppu", "Hita", "Nakatsu", "Ōita", "Saiki", "Usa", "Usuki"],
+  },
+  {
+    province: "Okayama",
+    city: ["Kasaoka", "Kurashiki", "Okayama", "Tamano", "Tsuyama"],
+  },
+  {
+    province: "Ōsaka",
+    city: [
+      "Daitō",
+      "Higashiōsaka",
+      "Hirakata",
+      "Ikeda",
+      "Izumiōtsu",
+      "Izumisano",
+      "Kadoma",
+      "Kishiwada",
+      "Matsubara",
+      "Moriguchi",
+      "Neyagawa",
+      "Ōsaka",
+      "Sakai",
+      "Suita",
+      "Takatsuki",
+      "Toyonaka",
+      "Yao",
+    ],
+  },
+  {
+    province: "Okinawa",
+    city: ["Naha", "Okinawa"],
+  },
+  {
+    province: "Saga",
+    city: ["Imari", "Karatsu", "Saga", "Tosu"],
+  },
+  {
+    province: "Saitama",
+    city: [
+      "Ageo",
+      "Asaka",
+      "Chichibu",
+      "Fukaya",
+      "Gyōda",
+      "Iruma",
+      "Kawagoe",
+      "Kawaguchi",
+      "Koshigaya",
+      "Kumagaya",
+      "Saitama",
+      "Sōka",
+      "Toda",
+      "Warabi",
+    ],
+  },
+  {
+    province: "Shiga",
+    city: ["Hikone", "Ōtsu", "Shigaraki"],
+  },
+  {
+    province: "Shimane",
+    city: ["Izumo", "Masuda", "Matsue"],
+  },
+  {
+    province: "Shizuoka",
+    city: [
+      "Atami",
+      "Fuji",
+      "Fujieda",
+      "Fujinomiya",
+      "Gotemba",
+      "Hamakita",
+      "Hamamatsu",
+      "Itō",
+      "Mishima",
+      "Numazu",
+      "Shimada",
+      "Shimizu",
+      "Shizuoka",
+      "Yaizu",
+    ],
+  },
+  {
+    province: "Tochigi",
+    city: ["Ashikaga", "Kanuma", "Nikkō", "Oyama", "Sano", "Utsunomiya"],
+  },
+  {
+    province: "Tokushima",
+    city: ["Anan", "Komatsushima", "Naruto", "Tokushima"],
+  },
+  {
+    province: "Tokyo",
+    city: [
+      "Chōfu",
+      "Fuchū",
+      "Ginza",
+      "Hachiōji",
+      "Higashimurayama",
+      "Hino",
+      "Kodaira",
+      "Koganei",
+      "Kokubunji",
+      "Machida",
+      "Mitaka",
+      "Musashino",
+      "Ōme",
+      "Tachikawa",
+      "Tokyo",
+    ],
+  },
+  {
+    province: "Tottori",
+    city: ["Kurayoshi", "Yonago"],
+  },
+  {
+    province: "Toyama",
+    city: ["Takaoka"],
+  },
+  {
+    province: "Wakayama",
+    city: ["Kainan", "Nachi-katsuura", "Wakayama"],
+  },
+  {
+    province: "Yamagata",
+    city: ["Sakata", "Tsuruoka", "Yamagata", "Yonezawa"],
+  },
+  {
+    province: "Yamaguchi",
+    city: [
+      "Hagi",
+      "Hōfu",
+      "Iwakuni",
+      "Kudamatsu",
+      "Shimonoseki",
+      "Tokuyama",
+      "Ube",
+    ],
+  },
+];
+
 export default function RegisterInformation({ params }) {
   const router = useRouter();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [gender, setGender] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [isPhoneValid, setIsPhoneValid] = useState(false);
   const [address, setAddress] = useState("");
+  const [provinceIndex, setProvinceIndex] = useState(0);
+  const [cityIndex, setCityIndex] = useState(0);
+  const [detailAdress, setDetailAdress] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const encodeEmail = params.verify_email;
@@ -129,6 +489,23 @@ export default function RegisterInformation({ params }) {
     });
   }
 
+  const handleChangePhoneNumber = (value) => {
+    const formattedPhoneNumber = value.startsWith("+") ? value : `+${value}`;
+    setPhoneNumber(formattedPhoneNumber);
+    setIsPhoneValid(validatePhoneNumber(formattedPhoneNumber));
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    const phoneNumberPattern = /^\+\d{1,3}(\d{9,11})$/;
+    return phoneNumberPattern.test(phoneNumber);
+  };
+
+  useEffect(() => {
+    setAddress(
+      `${location[provinceIndex].province}, ${location[provinceIndex].city[cityIndex]}, ${detailAdress}`
+    );
+  }, [provinceIndex, cityIndex, detailAdress]);
+
   return (
     <div className="register_information_container">
       <h1>Please inform you information for creating account</h1>
@@ -150,21 +527,74 @@ export default function RegisterInformation({ params }) {
                   onChange={(e) => setLastName(e.target.value)}
                 />
               </div>
+              <div>
+                <h4>
+                  Name : {firstName} {lastName}
+                </h4>
+                <h4>Address: {address}</h4>
+                <h4>Phone Number :{phoneNumber}</h4>
+                <h4>Gender: {gender}</h4>
+                <p>Please check your information correctly</p>
+              </div>
             </div>
             <div className="right_of_first">
               <div>
                 <label>Phone Number</label>
-                <input
-                  type="text"
-                  onChange={(e) => setPhoneNumber(e.target.value)}
+                <PhoneInput
+                  country={"jp"}
+                  enableSearch={true}
+                  value={phoneNumber}
+                  onChange={(phone) => handleChangePhoneNumber(phone)}
                 />
+                {!isPhoneValid && phoneNumber && (
+                  <p className="error">
+                    Invalid phone number. Ensure it has 9-11 digits after the
+                    country code.
+                  </p>
+                )}
               </div>
               <div>
                 <label>Address</label>
-                <input
-                  type="text"
-                  onChange={(e) => setAddress(e.target.value)}
-                />
+                <div className="selectContainer">
+                  <p>Province</p>
+                  <select
+                    value={provinceIndex}
+                    onChange={(e) => {
+                      setProvinceIndex(e.target.value);
+                    }}
+                  >
+                    {location.map((item, index) => {
+                      return (
+                        <option key={index} value={index}>
+                          {item.province}
+                        </option>
+                      );
+                    })}
+                  </select>
+                  <p>City</p>
+                  <select
+                    value={cityIndex}
+                    onChange={(e) => {
+                      setCityIndex(e.target.value);
+                    }}
+                  >
+                    {location[provinceIndex].city.map((item, index) => {
+                      return (
+                        <option key={index} value={index}>
+                          {item}
+                        </option>
+                      );
+                    })}
+                  </select>
+                  <p>Detail address</p>
+                  <input
+                    type="text"
+                    value={detailAdress}
+                    onChange={(e) => {
+                      setDetailAdress(e.target.value);
+                    }}
+                  ></input>
+                </div>
               </div>
             </div>
           </div>
@@ -194,6 +624,15 @@ export default function RegisterInformation({ params }) {
                   ></input>
                   <p>Female</p>
                 </div>
+                <div>
+                  <input
+                    type="radio"
+                    name="other"
+                    value="Other"
+                    onChange={(e) => setGender(e.target.value)}
+                  ></input>
+                  <p>Other</p>
+                </div>
               </div>
             </div>
           </div>
@@ -215,7 +654,13 @@ export default function RegisterInformation({ params }) {
           </div>
         </div>
         <div className="form_register_btn">
-          <button className="btn_clear">Clear</button>
+          <button
+            type="button"
+            className="btn_clear"
+            onClick={() => console.log(phoneNumber)}
+          >
+            Clear
+          </button>
           <button className="btn_finish" type="submit">
             Finish
           </button>
